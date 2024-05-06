b254a1b447bc6b8ce58bb7229e2f035e090e3de7
unknown <alexadoniev@gmail.com>
2024-05-06 13:21:06 +0300

    fix styles and animations

--- main.js
+++ main.js
@@ -1,24 +1,100 @@
-const logo = document.querySelector('.logo-container');
-const nav = document.querySelector('.nav-header');
+document.addEventListener("DOMContentLoaded", function () {
+  const logo = document.querySelector(".logo-container");
+  const nav = document.querySelector(".nav-header");
+  const topxa = document.querySelector(".top");
+  const headerxa = document.querySelector("header.header");
+  const form = document.querySelector("form");
 
-document.addEventListener('DOMContentLoaded', function() {
   function changeNavPosition() {
-    if (window.pageYOffset > 0) {
-      logo.style.position = 'fixed';
-      logo.style.marginTop = '0';
-      nav.style.position = 'fixed'
-      nav.style.marginTop = '0';
-      nav.style.width = `${window.innerWidth - logo.offsetWidth}px`;
-    }
-    
-    if (window.pageYOffset === 0) {
-      logo.style.position = 'relative';
-      logo.style.marginTop = '50px';
-      nav.style.width = '100%';
-      nav.style.position = 'relative'
-      nav.style.marginTop = '22px';
+    if (window.scrollY > 5) {
+      headerxa.style.paddingTop = topxa.getBoundingClientRect().height + "px";
+      topxa.classList.add("fixed");
+    } else {
+      topxa.classList.remove("fixed");
+      headerxa.style.paddingTop = "0";
     }
   }
-  
-  document.addEventListener('scroll', changeNavPosition)
-})
+
+  document.addEventListener("scroll", changeNavPosition);
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const firstName = document.querySelector('input[name="name"]');
+    const lastName = document.querySelector('input[name="lastName"]');
+    const email = document.querySelector('input[name="email"]');
+    const phone = document.querySelector('input[name="phone"]');
+
+    const btn = document.querySelector("form button[type='submit']");
+    btn.innerHTML = '<div class="loading"></div>';
+    fetch(
+      "https://script.google.com/macros/s/AKfycbwbZCOYhvjjVxar35EvLBhTCSz07A7VhsT-sKOlVZZBe8LGBchildvdCthsSUv6OkYK/exec",
+      {
+        method: "POST",
+        mode: "no-cors",
+        redirect: "follow",
+        cache: "no-cache",
+        headers: new Headers({
+          "Content-Type": "application/json",
+        }),
+        body: JSON.stringify({
+          firstName: firstName.value,
+          lastName: lastName.value,
+          email: email.value,
+          phone: phone.value,
+          site: "myeasyslots",
+        }),
+      }
+    )
+      .then(() => {
+        btn.innerHTML = "✓";
+      })
+      .finally(() => {
+        setTimeout(() => {
+          btn.innerHTML = "SUBMIT";
+          phone.value = null;
+          email.value = null;
+          lastName.value = null;
+          firstName.value = null;
+        }, 1500);
+      });
+  });
+
+  const modalsContainers = ["privacy-modal", "terms-modal", "cookie-modal"].map(
+    (id) => document.getElementById(id)
+  );
+  const modalButtons = ["privacy-btn", "terms-btn", "cookies-btn"].map((id) =>
+    document.getElementById(id)
+  );
+  const cancelButton = document.querySelectorAll(".cancelModal");
+
+  modalButtons.forEach((el, idx) =>
+    el.addEventListener("click", (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      modalsContainers[idx].showModal();
+      modalsContainers[idx].scroll({ top: 0 });
+    })
+  );
+
+  modalsContainers.forEach((el) => {
+    el.querySelector(".modal-content").addEventListener("click", (e) =>
+      e.stopPropagation()
+    );
+    el.addEventListener("click", () => el.close());
+  });
+
+  cancelButton.forEach((el) =>
+    el.addEventListener("click", () => {
+      modalsContainers.forEach((el) => el.close("animalNotChosen"));
+    })
+  );
+
+  document
+    .querySelectorAll("span.domain")
+    .forEach((el) => void (el.innerText = window.location.hostname));
+
+  document
+    .querySelectorAll("span.hostname")
+    .forEach((el) => void (el.innerText = window.location.hostname));
+});
